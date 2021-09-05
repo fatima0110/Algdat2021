9f69ddbe316daa3e58f7be761e7af9a54de7e977
fatima0110 <71825590+fatima0110@users.noreply.github.com>
2021-09-05 21:53:51 +0200

    endringer

---
+++ src/Ukeoppgaver/Uke1/Seksjon112/Oppgave3.java
@@ -0,0 +1,20 @@
+package Ukeoppgaver.Uke1.Seksjon112;
+
+public class Oppgave3 {
+    public static int maks(int[] a)  // a er en heltallstabell
+    {
+        if (a.length < 1)
+            throw new java.util.NoSuchElementException("a er tom");
+
+        int m = a.length - 1;  // indeks til største verdi
+
+        for (int i = a.length - 2; i >= 0; i--) // starter nest bakerst
+        {
+            if (a[i] > a[m]) m = i;  // indeksen oppdateres
+        }
+
+        return m;  // returnerer indeksen/posisjonen til største verdi
+
+    } // maks
+
+}
